354804a0fecdab51beea4571dcaadcfdc3bf1620
Patrick Mennen <patrick.mennen@philips.com>
2021-12-01 14:23:03 +0100

    prettier

--- 01/01.ts
+++ 01/01.ts
@@ -1,33 +1,31 @@
-import fs from "fs/promises";
-import * as R from 'ramda';
+import fs from 'fs/promises';
+import { pipe, aperture, filter, prop, map, sum } from 'ramda';
 
-const countTotal = R.pipe(
-    R.aperture(2),
-    R.filter((pred) => {
-        if (Array.isArray(pred)) {
-            return pred[1] > pred[0];
-        }
-        return false;
-    }),
-    R.prop('length')
+const countTotal = pipe(
+  aperture(2),
+  filter((pred) => {
+    if (Array.isArray(pred)) {
+      return pred[1] > pred[0];
+    }
+    return false;
+  }),
+  prop('length'),
 );
 
-const countWindowedTotal = R.pipe(
-    R.map(Number),
-    R.aperture(3),
-    R.map(R.sum),
-    countTotal
-)
+const countWindowedTotal = pipe(map(Number), aperture(3), map(sum), countTotal);
 
-const solutionOne = async (): Promise<void> =>  {
-    const input = await fs.readFile('./input.txt');
-    const points = input.toString().split('\n').map(input => Number(input));
-    console.log(`Part 1: `, countTotal(points))
-    console.log(`Part 2: `, countWindowedTotal(points));
-}
+const solutionOne = async (): Promise<void> => {
+  const input = await fs.readFile('./input.txt');
+  const points = input
+    .toString()
+    .split('\n')
+    .map((input) => Number(input));
+  console.log(`Part 1: `, countTotal(points));
+  console.log(`Part 2: `, countWindowedTotal(points));
+};
 
 const main = async () => {
-    await solutionOne();
-}
+  await solutionOne();
+};
 
 main();
